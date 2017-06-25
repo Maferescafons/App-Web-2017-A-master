dc42b9ec7a38249beb01b43b3f135f1b77aa6146
Maferescafons <escafons@hotmail.com>
2017-06-25 15:16:53 -0500

    metodos sail

--- 07-Backend-MVC/CarritoCompras/api/controllers/SaludoController.js
+++ 07-Backend-MVC/CarritoCompras/api/controllers/SaludoController.js
@@ -4,6 +4,7 @@
 /**
  * Created by USRDEL on 19/6/17.
  */
+var hola= hola
 module.exports = {
     welcome: function (req, res) {
         sails.log.info(req.method);
@@ -14,19 +15,40 @@ module.exports = {
             return res.send("Error");
         }
     },
-
     bienvenido: function (req, res) {
         //PUT
-      sails.log.info(req.method);
-      if (req.method == "PUT") {
         return res.send("Hola");
-      }
-      else {
-        return res.send("Error");
-      }
-
     },
-  ///// req.acceptedCharsets--------------------
+
+  ////req.host----The hostname of this request, without the port number, as specified by its "Host" header.
+  reqHost: function (req, res){
+    sails.log.info(req.headers.host)
+  },
+
+  reqParam:function(req, res) {
+
+  var user_id = req.param('id');
+  var token = req.param('token');
+  var geo = req.param('geo');
+  req.param('sku');
+
+  res.send(user_id + ' ' + token + ' ' + geo);
+},
+  //req.get() Returns the specified HTTP request header field (case-insensitive match). The Referrer and Referer fields are interchangeable.
+  reqGet:function(req, res) {
+    sails.log.info(req.get('Something'))
+  },
+  //req.accepts () Comprueba si la lista indicada de esta solicitud de "aceptado" tipos de medios incluye el especificado type. Devuelve verdadero o falso.
+  reqAccepts: function(req, res) {
+    sails.log.info(req.accepts('application/json'))
+    sails.log.info(req.accepts('json'))
+  },
+  //req.protocol The protocol used to send this request (req).
+  reqProtocol:function(req, res) {
+    sails.log.info(req.protocol)
+  },
+  //req.acceptedCharsetsThis property is an array that contains the acceptable charsets specified by the user agent in the request.
+
   acceptedCharsets: function (req, res) {
     sails.log.info(req.acceptedCharsets, ['utf-8', 'utf-7', 'iso-8859-1']);
     if (req.acceptedCharsets =='utf-8' ){
@@ -35,28 +57,23 @@ module.exports = {
     else {
       return res.send("Error");
     }
-  },
-
-  /////req.fresh--Una bandera que indica el envío de esta solicitud de agente de usuario ( req) quiere datos "frescos" (como se indica por el " si-no-match ", " Cache-Control " y / o " If-Modified-ya " los encabezados de solicitud.)
-
-  //Si la solicitud quiere datos "frescos", por lo general usted querrá .find()datos frescos de sus modelos y la envían de vuelta al cliente.
-
-  fresh: function (req, res){
-      sails.log.info(req.fresh)
-    if (req.fresh){
-      // El usuario-agente solicita una versión más actualizada del recurso solicitado.
-      // Vamos a la base de datos para obtener algunas cosas y enviarlo de vuelta.
-    }
-    else
-    {
-      return res.send("El usuario no ha solicitado nada");
+    if (req.acceptedCharsets =='utf-7' ){
+      return res.send("utf-7");
     }
   },
+ // req.acceptedLanguages Una matriz que contiene los idiomas de respuesta "aceptables" especificados por el agente de usuario en el " Accept-Language cabecera" de esta solicitud ( req).
 
-  ////req.host----The hostname of this request, without the port number, as specified by its "Host" header.
-  reqHost: function (req, res){
-    sails.log.info(req.headers.host)
+  acceptedLanguages:function (req, res) {
+    sails.log.info(req.acceptedLanguages)
+    return res.send('es-ES es');
   },
-  
+
+  allParamm: function (req, res) {
+
+    sails.log.info(req.all)
+
+  },
 
 };
+
+
